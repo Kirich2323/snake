d08531a8cf9e122203724985d5725015971c7547
Vadik <ivadik2323@gmail.com>
2016-05-20 23:53:53 +1000

    Add more constant

--- Constants.h
+++ Constants.h
@@ -5,8 +5,12 @@
 #define SNAKE_SYMBOL '#'
 #define SPACE_SYMBOL ' '
 #define APPLE_SYMBOL '.'
+#define VERTICAL_BORDER '|'
+#define HORIZONTAL_BORDER '-'
 
 #define SCORE_PER_APPLE 10
+#define TURN_TIME_MILISECONDS 400
+#define MIN_TURN_TIME_MILISECONDS 150
 
 std::map<int, vec2i> directions = { 
 	{77, vec2i(1, 0)},
